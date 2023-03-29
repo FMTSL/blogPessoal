3d5e99008d70f37f9081aa64465e9c107359c4c2
Felipe Matos <88333095+FMTSL@users.noreply.github.com>
2023-03-28 23:23:27 -0300

    Criação e configuração do projeto Blog Pessoal -28/03/2023

---
+++ blogPessoal/src/test/java/com/FelipeMatosLima/blogPessoal/BlogPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FelipeMatosLima.blogPessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
