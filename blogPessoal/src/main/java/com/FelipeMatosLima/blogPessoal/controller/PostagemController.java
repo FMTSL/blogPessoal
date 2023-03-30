4133f9fbe810d28a6d78ff9ea09c8d17fbbd82b3
Felipe Matos <88333095+FMTSL@users.noreply.github.com>
2023-03-29 21:45:14 -0300

    Inserção do método PUT Spring - 29/03/2023

--- blogPessoal/src/main/java/com/FelipeMatosLima/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/com/FelipeMatosLima/blogPessoal/controller/PostagemController.java
@@ -36,4 +36,12 @@ public class PostagemController {
         return  ResponseEntity.status(HttpStatus.CREATED)
                 .body(postagemRepository.save(postagem));
     }
+    // Criação do método PUT
+    @PutMapping
+    public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+        return postagemRepository.findById(postagem.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK)
+                        .body(postagemRepository.save(postagem)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 }
