dae579386bafedcf4908aed973cafa461f488a6c
Felipe Matos <88333095+FMTSL@users.noreply.github.com>
2023-03-29 21:22:58 -0300

    Inserção do método Post Spring - 29/03/2023

--- blogPessoal/src/main/java/com/FelipeMatosLima/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/com/FelipeMatosLima/blogPessoal/controller/PostagemController.java
@@ -2,7 +2,9 @@ package com.FelipeMatosLima.blogPessoal.controller;
 
 import com.FelipeMatosLima.blogPessoal.model.Postagem;
 import com.FelipeMatosLima.blogPessoal.repository.PostagemRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,7 @@ public class PostagemController {
     @Autowired //Responsável por fazer a injeção de dependência do Spring, trazendo os métodos criados da repository
     private PostagemRepository postagemRepository;
 
+    // Criação do método GET
     @GetMapping
     public ResponseEntity<List<Postagem>> getAll(){
         return ResponseEntity.ok(postagemRepository.findAll());
@@ -26,5 +29,11 @@ public class PostagemController {
         return postagemRepository.findById(id)
                 .map(resposta -> ResponseEntity.ok(resposta))
                 .orElse(ResponseEntity.notFound().build());
+    }
+    // Criação do método POST
+    @PostMapping
+    public ResponseEntity<Postagem> post (@Valid @RequestBody Postagem postagem) {
+        return  ResponseEntity.status(HttpStatus.CREATED)
+                .body(postagemRepository.save(postagem));
     }
 }
